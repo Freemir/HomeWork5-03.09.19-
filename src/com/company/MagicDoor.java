9be08b58a0c5d4ed4dbc70eabb1c07e0e54fedcc
Freemir <werattest@gmail.com>
2019-09-10 19:11:12 +0600

    Изменил имена героев с верхнего регистра в нижний

--- src/com/company/MagicDoor.java
+++ src/com/company/MagicDoor.java
@@ -1,12 +1,13 @@
 package com.company;
 
 public class MagicDoor {
-    Heroes Warrior = new Heroes(250,20,1);
-    Heroes Magic = new Heroes(250, 20,2);
-    Heroes Archer = new Heroes(250,20, 3);
-    Heroes Shaman = new Heroes(250, 20,4);
+    Heroes warrior = new Heroes(250,20,1);
+    Heroes magic = new Heroes(250, 20,2);
+    Heroes archer = new Heroes(250,20, 3);
+    Heroes shaman = new Heroes(250, 20,4);
+
     public Heroes[] returnHeroes (){
-        Heroes[] arrHeroes = {Warrior, Magic, Archer, Shaman};
+        Heroes[] arrHeroes = {warrior, magic, archer, shaman};
         return arrHeroes;
     }
 }
